6066ca1c4ad76c55cb5fb1a55ad8ae6f96874df5
Devansh Awasthi <136520797+dtgprogrammer@users.noreply.github.com>
2024-09-08 02:13:02 +0530

    Update harmony.js

--- pages/projects/harmony.js
+++ pages/projects/harmony.js
@@ -31,11 +31,11 @@ function Harmony() {
                         <div className="mx-8 justify-center lg:justify-start flex lg:ml-40 mt-8 ">
 
                             <div>
-                                <Link href={'https://github.com/ayush6678/Harmony'}>
+                                <Link href={'https://github.com/dtgprogrammer/harmony'}>
                                     <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Github Frontend</button>
                                 </Link>
 
-                                <Link href={'https://github.com/ayush6678/Harmony-Backend'}>
+                                <Link href={'https://github.com/dtgprogrammer/Harmony-backend'}>
                                     <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Github Backend</button>
                                 </Link>
 
